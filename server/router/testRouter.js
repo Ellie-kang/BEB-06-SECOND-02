9e447652b264302b055086e0d122dbdf11736ccb
HCW <jingi6052@naver.com>
2022-10-27 11:44:27 +0900

    server

---
+++ server/router/testRouter.js
@@ -0,0 +1,9 @@
+const { findAll, approve } = require('../controller/test');
+const express = require('express');
+const router = express.Router();
+
+router.get('/', findAll);
+
+router.get('/approve', approve);
+
+module.exports = router;
