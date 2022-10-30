d1006b5a5bda110dc15eff1839073c4dcb39d394
HCW <jingi6052@naver.com>
2022-10-30 17:24:32 +0900

    WritePage, DetailPage

--- client/src/Pages/WritePage.js
+++ client/src/Pages/WritePage.js
@@ -1,9 +1,90 @@
-import React from 'react'
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Paper from '@mui/material/Paper';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import WriteForm from '../components/WriteForm';
+import Review from '../components/Review';
+import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom'
 
-const WritePage = () => {
+
+const theme = createTheme();
+
+export default function WritePage() {
+  const [activeStep, setActiveStep] = React.useState(0);
+  const [title, setTitle] = React.useState('');
+  const [content, setContent] = React.useState('');
+
+  const steps = ['글쓰기', '미리보기', '작성 완료'];
+
+
+  function getStepContent(step) {
+    switch (step) {
+      case 0:
+        return <WriteForm activeStep={activeStep} steps={steps} handleNext={handleNext} setTitle={setTitle} setContent={setContent} title={title} content={content} />;
+      case 1:
+        return <Review activeStep={activeStep} handleBack={handleBack} handleNext={handleNext} title={title} content={content} />;
+      case 2:
+        return (<React.Fragment>
+          <Typography variant="h5" gutterBottom>
+            게시글 작성이 완료되었습니다.
+          </Typography>
+          <Link to="/">
+            <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+              <Button variant="contained" sx={{ mt: 3, ml: 1 }}>
+                HOME
+              </Button>
+            </Box>
+          </Link>
+        </React.Fragment>)
+      default:
+        throw new Error('Unknown step');
+    }
+  }
+
+  const handleNext = () => {
+    if (title == '' || content == '') {
+      alert('빈칸을 채워주세요');
+    }
+    else {
+      if (activeStep == 1) { //db에 저장하는 코드를 실행하는 곳
+        console.log('title : ' + title);
+        console.log('content : ' + content);
+      }
+      setActiveStep(activeStep + 1);
+    }
+  };
+
+  const handleBack = () => {
+    setActiveStep(activeStep - 1);
+  };
+
   return (
-    <div>WritePage</div>
-  )
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container component="main" maxWidth="md" sx={{ mb: 4 }}>
+        <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
+          <Typography component="h1" variant="h4" align="center">
+            게시글 작성하기
+          </Typography>
+          <Stepper activeStep={activeStep} sx={{ pt: 3, pb: 5 }}>
+            {steps.map((label) => (
+              <Step key={label}>
+                <StepLabel>{label}</StepLabel>
+              </Step>
+            ))}
+          </Stepper>
+          <React.Fragment>
+            {getStepContent(activeStep)}
+          </React.Fragment>
+        </Paper>
+      </Container>
+    </ThemeProvider>
+  );
 }
-
-export default WritePage
---
+++ client/src/components/Review.js
@@ -0,0 +1,57 @@
+import * as React from 'react';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+
+const information = [
+    { name: '이름', detail: 'blablabla' },
+    { name: '작성일자', detail: '2020.03.12' },
+];
+
+export default function Review(props) {
+    return (
+        <React.Fragment>
+            <Typography variant="h6" gutterBottom>
+                {props.title}
+            </Typography>
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                    <Typography variant="h6" gutterBottom sx={{ mt: 2 }}>
+                        {props.content}
+                    </Typography>
+                </Grid>
+                <Grid item container direction="column" xs={12}>
+                    <Typography variant="h6" gutterBottom sx={{ mt: 2 }}>
+                        작성자
+                    </Typography>
+                    <Grid container>
+                        {information.map((el) => (
+                            <React.Fragment key={el.name}>
+                                <Grid item xs={6}>
+                                    <Typography gutterBottom>{el.name}</Typography>
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <Typography gutterBottom>{el.detail}</Typography>
+                                </Grid>
+                            </React.Fragment>
+                        ))}
+                    </Grid>
+
+                </Grid>
+            </Grid>
+            <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+                <Button onClick={props.handleBack} sx={{ mt: 3, ml: 1 }}>
+                    Back
+                </Button>
+                <Button
+                    variant="contained"
+                    onClick={props.handleNext}
+                    sx={{ mt: 3, ml: 1 }}
+                >
+                    {props.activeStep == 1 ? 'Complete' : 'Next'}
+                </Button>
+            </Box>
+        </React.Fragment>
+    );
+}
