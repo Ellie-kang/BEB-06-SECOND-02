09d19a390903fa076f78f2d14b5e0112a2e23390
Heein-Park <gmldls94@gmail.com>
2022-10-31 11:20:03 +0900

    feat: authentication

--- server/controller/users.js
+++ server/controller/users.js
@@ -1,55 +1,55 @@
+require('dotenv').config();
 const User = require('../model/user');
+const jwt = require('jsonwebtoken');
 
 const find = async (req, res) => {
-  const user = await User.find();
+  const _queries = req.query;
+  const user = await User.find(_queries, 'userId profile_image created_at account');
   res.send(user);
 };
 
 const signup = async (req, res) => {
-  const { userId, password, salt } = req.body;
+  const { userId, password } = req.body;
 
   // 여기 있는 account 데이터는 추후 이더리움 노드와 연동되면 채워질 부분입니다.
   const user = new User({
     userId,
     password,
-    salt,
     account: null
   });
 
+  // user 모델에서 mongoose-unique-validator 플러그인을 적용한 채로
   // User Model에서 userID에 unique 옵션을 설정했기 때문에
   // validate() 함수에서 유효성 조사하는 과정 중 이중 아이디를 검출가능
-  user.validate().then(
-    async (error) => {
-      if (error) {
-        res.status(400).send({ error });
-      } else {
-        const newDocument = await user.save();
-        res.status(201).send(newDocument);
-      }
-    }
-  );
+  try {
+    const validation = user.validateSync();
+    if (validation) throw validation.errors;
+
+    const newDocument = await user.save();
+    res.status(201).send(newDocument);
+  } catch (errors) {
+    res.status(400).send({ errors });
+  }
 };
 
 const login = async (req, res) => {
+  const { userId, password } = req.body;
+
+  try {
+    const user = await User.findOne({ userId });
+    const result = await user.compare(password, user.password);
+    if (!user || !result) throw new Error('Authentication failed. Invalid user or password.');
+    else {
+      const token = jwt.sign({ userId }, process.env.SECRET, { expiresIn: '1h' });
+      res.json({ token });
+    }
+  } catch (err) {
+    res.status(401).send({ message: err.message });
+  }
+};
+
+const uploadProfile = async (req, res) => {
   res.status(501).send();
-  // const { title, content, author } = req.body;
-
-  // const article = new Article({
-  //   title,
-  //   content,
-  //   author
-  // });
-
-  // article.validate().then(
-  //   async (error) => {
-  //     if (error) {
-  //       res.status(400).send({ error });
-  //     } else {
-  //       const newDocument = await article.save();
-  //       res.status(201).send(newDocument);
-  //     }
-  //   }
-  // );
 };
 
 module.exports = {
--- server/model/user.js
+++ server/model/user.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+const bcrypt = require('bcrypt');
+const SALT_WORK_FACTOR = 10;
 
 const schema = new mongoose.Schema({
   userId: {
@@ -10,10 +13,29 @@ const schema = new mongoose.Schema({
     type: String,
     required: true
   },
-  salt: String,
   profile_image: Buffer,
+  created_at: Date,
   account: String
 });
+
+schema.plugin(uniqueValidator);
+
+// save 직전 작동하는 pre hook 함수
+schema.pre('save', function (next) {
+  // 생성 날짜를 자동 기입함
+  if (!this.created_at) this.created_at = new Date();
+
+  // 비밀번호 해싱
+  if (this.isModified('password')) {
+    const salt = bcrypt.genSaltSync(SALT_WORK_FACTOR);
+    const encrypted = bcrypt.hashSync(this.password, salt);
+    this.password = encrypted;
+  }
+
+  next();
+});
+
+schema.methods.compare = bcrypt.compare;
 
 const User = mongoose.model('User', schema, 'Takoyaki-User');
 
