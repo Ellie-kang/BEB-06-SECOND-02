e180d8a594e78b81a5f8d6dfb0297db36a21ace7
Heein-Park <gmldls94@gmail.com>
2022-11-01 10:59:04 +0900

    feat: comment api

--- server/controller/articles.js
+++ server/controller/articles.js
@@ -1,4 +1,5 @@
 const Article = require('../model/article');
+const Comment = require('../model/comment');
 
 const find = async (req, res) => {
   const _queries = req.query;
@@ -26,7 +27,29 @@ const write = async (req, res) => {
   }
 };
 
+const comment = async (req, res) => {
+  console.log(req);
+  const { content, postId, userId } = req.body;
+
+  const comment = new Comment({
+    content,
+    postId,
+    userId
+  });
+
+  try {
+    const validation = comment.validateSync();
+    if (validation) throw validation.errors;
+
+    const newDocument = await comment.save();
+    res.status(201).send(newDocument);
+  } catch (errors) {
+    res.status(400).send({ errors });
+  }
+};
+
 module.exports = {
   find,
   write
+  comment
 };
---
+++ server/model/comment.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+  userId: {
+    type: mongoose.ObjectId,
+    required: true
+  },
+  postId: {
+    type: mongoose.ObjectId,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  created_at: Date
+});
+
+// save 직전 작동하는 pre hook 함수
+schema.pre('save', function (next) {
+  // 생성 날짜를 자동 기입함
+  if (!this.created_at) this.created_at = new Date();
+
+  next();
+});
+
+const Comment = mongoose.model('Comment', schema, 'Takoyaki-Comment');
+
+module.exports = Comment;
