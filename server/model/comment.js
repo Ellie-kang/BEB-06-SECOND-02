6ccffd0a63815eec34b1d9b75554104d0cb23f2a
Heein-Park <gmldls94@gmail.com>
2022-11-02 22:15:57 +0900

    refactor: edit camelCase

--- server/model/comment.js
+++ server/model/comment.js
@@ -13,13 +13,13 @@ const schema = new mongoose.Schema({
     type: String,
     required: true
   },
-  created_at: Date
+  createdAt: Date
 });
 
 // save 직전 작동하는 pre hook 함수
 schema.pre('save', function (next) {
   // 생성 날짜를 자동 기입함
-  if (!this.created_at) this.created_at = new Date();
+  this.createdAt = new Date();
   next();
 });
 
