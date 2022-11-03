6dfc53b4165c35f082645502b2d2a42761064e80
Heein-Park <gmldls94@gmail.com>
2022-11-03 20:12:25 +0900

    refactor: $match order fix

--- server/controller/users.js
+++ server/controller/users.js
@@ -7,7 +7,6 @@ const { main, transferFrom } = require('./web3');
 const find = async (req, res) => {
   const _queries = req.query;
   const user = await User.aggregate([
-    { $match: _queries },
     {
       $lookup: {
         from: 'Takoyaki-Comment',
@@ -40,7 +39,8 @@ const find = async (req, res) => {
         as: 'articles'
       }
     },
-    { $project: { password: 0 } }
+    { $project: { password: 0 } },
+    { $match: _queries }
   ]);
   res.send(user);
 };
