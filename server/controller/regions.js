a9d10f092ba37ea9591dc33785d7664a82e979f8
Heein-Park <gmldls94@gmail.com>
2022-11-07 13:34:37 +0900

    refactor: remove unnecessary codes

--- server/controller/regions.js
+++ server/controller/regions.js
@@ -3,7 +3,6 @@ const Region = require('../model/region');
 
 const listByRegion = async (req, res) => {
   const _queries = req.query;
-  console.log(_queries);
   // 기존 아티클 도큐먼트에 다른 DB의 도큐먼트를 가져오는 코드
   const regions = await Region.aggregate([
     {
