8352ad5a6e266d89179be117600978d130e30f2e
Heein-Park <gmldls94@gmail.com>
2022-11-07 19:40:08 +0900

    fix: region order

--- server/controller/regions.js
+++ server/controller/regions.js
@@ -5,14 +5,21 @@ const listByRegion = async (req, res) => {
   const _queries = req.query;
   // 기존 아티클 도큐먼트에 다른 DB의 도큐먼트를 가져오는 코드
   const regions = await Region.aggregate([
+    { $sort: { region: 1, city: 1 } },
     {
       $group: {
         _id: '$region',
-        cities: { $addToSet: '$city' }
+        cities: { $push: '$city' }
       }
     },
-    { $addFields: { region: '$_id' } },
-    { $unset: '_id' },
+    {
+      $project: {
+        _id: 0,
+        region: '$_id',
+        cities: 1
+      }
+    },
+    { $sort: { region: 1 } },
     { $match: _queries }
   ]);
 
