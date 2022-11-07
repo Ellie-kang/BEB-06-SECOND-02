14f115f1b77e114cf933a86435b4a2d10c725ebe
Ellie-kang <kya6873@hanmail.net>
2022-11-07 19:07:25 +0900

    [client]like 수정 완

--- client/src/components/Like.js
+++ client/src/components/Like.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import IconButton from '@mui/material/IconButton';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
@@ -6,42 +6,47 @@ import Typography from '@mui/material/Typography';
 import axios from 'axios';
 import { Stack } from '@mui/system';
 import { AppContext } from "../AppContext";
-
+import styled from "styled-components";
 
 export const Like = (props) => {
   const [isLike, setIsLike] = useState(false);
-  const [color, setColor] = useState('unLike')
 
   const {like} = props;
 
   const context = useContext(AppContext);
   const { userId } = context.state;
-  console.log(userId)
-  console.log(like)
-
 
   const isLiked = (e) => {
     axios.patch('http://localhost:3001/articles/like', {
       articleId : props.articleId,
-    },{withCredentials : true})
+    },{ withCredentials : true })
     .then((res) => {
       if(isLike) {
         setIsLike(false);
-        setColor('unLike')
       } else {
         setIsLike(true);
-        setColor('like')
       }
       console.log(res)
-      // window.location.replace("/")
+      window.location.replace("/")
     }).catch((err) => {
       console.log(err)
     })
   }
 
+  // 좋아요 유무에 따라 새로고침 되도 그대로 반영함
+  useEffect(() => {
+    const _like = like.filter((data) => {
+      return data.userId === userId;  
+    })[0]
+    console.log(_like)
+    if(_like) {
+      setIsLike(true)
+    }
+  })
+  
   return (
     <Stack direction="row"> 
-    {isLike 
+    {isLike
       ? (
         <IconButton
           aria-label='add to favorites'
